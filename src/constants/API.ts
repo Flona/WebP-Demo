8656829c8e0466c45edb4a656408ab1986a4b92e
Mercle_梁 <lzz_only@hotmail.com>
2018-04-19 16:17:28 +0800

    refactor(http): 优化http

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,10 +1,18 @@
 // api 都集中在这里
-import {sendGet, sendPost, sendDelete, sendPut} from '../utils/base'
-let getUrl = 'http://www.zhiyinlou.com/theme/index.css'
+import { sendGet, sendPost, sendDelete, sendPut } from "../utils/base"
+let getUrl = "http://www.zhiyinlou.com/theme/index.css"
 // https://www.baidu.com/s?wd=%E6%B5%8B%E8%AF%95
-let deletUrl = 'https://jsonplaceholder.typicode.com/posts/'
+let deletUrl = "https://jsonplaceholder.typicode.com/posts/"
 // https://jsonplaceholder.typicode.com/posts/1
-export const testGetApi = (params ,id)=> { return sendGet(`${getUrl}`, params); }
-export const testDeleteApi = (params ,id)=> { return sendDelete(`${deletUrl}${id}`, params); }
-export const testPutApi = (params ,id)=> { return sendPut(`${deletUrl}${id}`, params); }
-export const testPostApi = (params ,id)=> { return sendPost(`${deletUrl}`, params); }
+export const testGetApi = (params: object, id: string) => {
+  return sendGet(`${getUrl}`, params)
+}
+export const testDeleteApi = (params: object, id: string) => {
+  return sendDelete(`${deletUrl}${id}`, params)
+}
+export const testPutApi = (params: object, id: string) => {
+  return sendPut(`${deletUrl}${id}`, params)
+}
+export const testPostApi = (params: object, id: string) => {
+  return sendPost(`${deletUrl}`, params)
+}
